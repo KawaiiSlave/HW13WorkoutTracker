57d7511b3d34800e8f4220e83004da1eaa2e8ea4
Tyler <tgandle@gmail.com>
2020-03-08 19:40:58 -0500

    changed port for testing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 
