86e21c217d7c2a78fdab63c210e639eb1b9e0b67
Tyler <tgandle@gmail.com>
2020-03-08 19:42:46 -0500

    changed model for our schema

---
+++ models/workout.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const opts = { toJSON: { virtuals: true }};
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: []
+});
+
+workoutSchema.virtual("totalDuration").get(function() {
+    let totalDuration = 0;
+    for (let i = 0; i < this.exercises.length; i++) {
+        totalDuration += this.exercises[i].duration;
+    }
+    return totalDuration;
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
