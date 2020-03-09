c5f16f993b610b18c0ba7f7362739531c4c01da0
Tyler <tgandle@gmail.com>
2020-03-08 19:36:33 -0500

    added api, and html routes

---
+++ routes/api-routes.js
@@ -0,0 +1,48 @@
+const api_router = require("express").Router();
+const db = require("../models");
+const mongojs = require("mongojs");
+
+api_router.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then(data => {
+        res.json(data);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+api_router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+        mongojs.ObjectId(req.params.id),
+        {
+            $push: {
+                exercises: req.body
+            }
+        },
+
+        (err, edited) => {
+            if (err) {
+                res.status(400).json(err);
+            } else {
+                res.send(edited);
+            }
+        });
+});
+
+api_router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create({ exercises: body })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        });
+});
+
+api_router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .sort({ date: 1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = api_router;
---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+const html_router = require("express").Router();
+const path = require("path");
+
+html_router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+html_router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+html_router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+module.exports = html_router;
